7870157186ceda8dcf2294fea99546b1b90b595a
Abedallatef <abedallatefqutami@gmail.com>
2021-06-30 09:46:12 -0700

    frst commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
+import './css/style.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'jquery/dist/jquery.min';
+import 'popper.js/dist/popper.min';
+import '@fortawesome/fontawesome-free/js/all.min';
+import 'webpack-jquery-ui';
+import 'webpack-jquery-ui/css';
+import 'jquery-ui-touch-punch/jquery.ui.touch-punch.min.js';
+
+
+
